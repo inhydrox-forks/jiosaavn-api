585ddc232544bf2cd17b10fb967bd37ec8cadcbc
Rajat Mishra <85543339+hydrox19@users.noreply.github.com>
2021-06-20 08:54:48 +0530

    Update validator.ts

--- utils/validator.ts
+++ utils/validator.ts
@@ -1,6 +1,8 @@
 export const extractIdFromLink = (link: string, type: string): string => {
   if (link.includes(`jiosaavn.com/${type}/`)) {
     return link.split(`${type}/`)[1].split("/")[1].slice(0, 11);
+  } else if (link.includes(`saavn.com/s/${type}/`)) {
+    return link.split(`${type}/`)[1].split("/")[3];
   }
   return "";
 };
